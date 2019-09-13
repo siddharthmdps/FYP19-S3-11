77297317027eea9d09c0029db4ae01e81eef6c64
Poh <minmarnoo@gmail.com>
2019-09-13 12:56:24 +0800

    setting up handlebars

--- pegasus_sim/src/app.js
+++ pegasus_sim/src/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const hbs = require('hbs')
+
 
 // Defining paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public') // frontend files HTML,CSS, frontend JS
+const viewsPath = path.join(__dirname, '../templates/views')  // handlebars templates
+const partialsPath = path.join(__dirname, '../templates/partials') // handlebars templates (headers, footers etc)
 const loginPath = path.join(__dirname, '../api/routes/login')
-const loginRoutes = require(loginPath); // const loginRoutes = require('../api/routes/login');
 
 
+
+// Setting up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Route handlers
 app.get('', (req, res) => {
@@ -14,6 +23,7 @@ app.get('', (req, res) => {
 })
 
 
+const loginRoutes = require(loginPath);
 app.use('/login', loginRoutes)
 
 
