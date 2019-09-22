1ff92a2927ea38b5c2786faceded578ebf27aa9b
Min San (AP) <min.san@global.ntt>
2019-09-22 11:30:34 +0800

    Changed mysql connection to pool

--- backend/api/routes/login.js
+++ backend/api/routes/login.js
@@ -7,7 +7,7 @@ const env = require('dotenv').config({
 const sha1 = require('sha1');
 const mysql = require('mysql');
 
-const myConnection = mysql.createConnection({
+const mypool = mysql.createPool({
     host: process.env.Db_Host,
     user: process.env.Db_User,
     password: process.env.Db_Password,
@@ -24,29 +24,34 @@ router.post('/', (req, res, next) => {
 router.post('/:username/:password', (req, res, next) => {
     const username = req.params.username;
     const password = sha1(req.params.password);
-    myConnection.connect();
+    mypool.getConnection(function(err,connection) {
+        if (err) {
+			connection.release();
+	  		console.log(' Error getting mysql_pool connection: ' + err);
+	  		throw err;
+	  	}
+        if (username && password) {
+            connection.query('SELECT FirstName, LastName, Email FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+                if (error) {
+                    res.status(500).json({
+                        message: error,
+                        results: results
+                    });
+                }
+                if (results && results.length > 0) {
+                    res.status(200).json({
+                    message: "Success! Hi, " + results[0].FirstName + "! Your Email Address is " + results[0].Email
+                    });    
+                }
+            });
+        } else {
+            res.status(400).json({
+                message: "Bad Request! Enter username and password!"
+            });
+        }
 
-    if (username && password) {
-		myConnection.query('SELECT FirstName, LastName, Email FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-            if (error) {
-                res.status(500).json({
-                    message: error,
-                    results: results
-                });
-            }
-            if (results && results.length > 0) {
-                res.status(200).json({
-                   message: "Success! Hi, " + results[0].FirstName + "! Your Email Address is " + results[0].Email
-                });    
-            }
-		});
-	} else {
-		res.status(400).json({
-            message: "Bad Request! Enter username and password!"
-        });
-    }
-
-    myConnection.end();
+        connection.release();     
+    });
 });
 
 module.exports = router;
