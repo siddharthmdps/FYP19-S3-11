6aad7248598686964930be84533f6b2493a960bb
Poh <minmarnoo@gmail.com>
2019-10-13 11:18:00 +0800

    backend

---
+++ backend/api/routes/employer/postjob.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const env = require('dotenv').config({
+    path: './src/Config.env'
+});
+
+const sha1 = require('sha1');
+const mysql = require('mysql');
+
+const mypool = mysql.createPool({
+    host: process.env.Db_Host,
+    user: process.env.Db_User,
+    password: process.env.Db_Password,
+    database: process.env.Db_Source,
+    port: process.env.Db_Port
+});
+
+router.post('/', (req, res, next ) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(req.body)
+})
--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,25 @@ const cors = require('cors')
 const loginPath = path.join(__dirname, '../api/routes/login')
 const loginRoutes = require(loginPath); // const loginRoutes = require('../api/routes/login');
 
+// General path
 const createUserPath = path.join(__dirname, '../api/routes/createUser')
 const createUserRoutes = require(createUserPath);
 
-const employerPath = path.join(__dirname, '../api/routes/employer')
+
+// Path for Employer
+const employerPath = path.join(__dirname, '../api/routes/employer/employer')
 const employerRoutes = require(employerPath);
 
+const postJobPath = path.join(__dirname, '../api/routes/employer/postjob')
+const postJobRoutes = require(postJobPath);
+
+// Path for Student
 const studentPath = path.join(__dirname, '../api/routes/students')
 const studentRoutes = require(studentPath);
+
+// Path for Admin
+
+
 
 // Route handlers
 app.get('', (req, res) => {
@@ -37,9 +48,10 @@ app.use(cors(options));
 
 app.use('/login', cors(options), loginRoutes)
 
-app.use('/createUser', options), createUserRoutes)
+app.use('/createUser', cors(options), createUserRoutes)
 
 app.use('/employer', cors(options), employerRoutes)
+app.use('/employer/postjob', cors(options), postJobRoutes)
 
 app.use('/students', cors(options), studentRoutes)
 
