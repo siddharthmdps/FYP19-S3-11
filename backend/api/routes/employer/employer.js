022aec467a188cfe9067174d050d2562f30f7bdf
Poh <minmarnoo@gmail.com>
2019-10-14 11:26:48 +0800

    utils

--- backend/api/routes/employer/employer.js
+++ backend/api/routes/employer/employer.js
@@ -1,19 +1,4 @@
-const express = require('express');
-const router = express.Router();
-
-const env = require('dotenv').config({
-    path: './src/Config.env'
-});
-const sha1 = require('sha1');
-const mysql = require('mysql');
-
-const mypool = mysql.createPool({
-    host: process.env.Db_Host,
-    user: process.env.Db_User,
-    password: process.env.Db_Password,
-    database: process.env.Db_Source,
-    port: process.env.Db_Port
-});
+const {express, router, env, sha1, mysql, mypool} = require('../../util')
 
 router.get('/',(req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
