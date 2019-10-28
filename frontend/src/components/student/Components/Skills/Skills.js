747f2e8fc661683e33d6ed3063e5542d4f8c7c9b
Joshua Oliver <j.oliverc18@gmail.com>
2019-10-29 04:04:49 +0800

    Skill: cursor, color, check null
    New color on hover, Auto focus curson on click, skill not added if null

--- frontend/src/components/student/Components/Skills/Skills.js
+++ frontend/src/components/student/Components/Skills/Skills.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import classes from './Skills.module.css';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 const skills = props => {
     return (
         <React.Fragment>
             {
-            props.details.Edit?<Button as="input" autoFocus={true} className={classes.Input} value={props.details.SkillName} onChange={props.changeFn} onKeyPress={props.changeFn} onBlur={props.removeEdit}/>:
-            <Button className={classes.Skill} onClick={props.remove} >{props.details.SkillName}</Button>
+                props.details.Edit ?
+                    <Button as="input"
+                        autoFocus={true}
+                        className={classes.Input} value={props.details.SkillName}
+                        onChange={props.changeFn}
+                        onKeyPress={props.changeFn} 
+                        onBlur={props.details.SkillName.trim() == "" ? props.remove : props.removeEdit} />
+                    :
+                    <Button className={classes.Skill} onClick={props.remove} >{props.details.SkillName}</Button>
             }
         </React.Fragment>
     );
