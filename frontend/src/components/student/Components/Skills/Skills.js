0e15670030f60edd73e3996c68a70a0f879321a4
Joshua Oliver <j.oliverc18@gmail.com>
2019-10-29 03:35:12 +0800

    Updated skills section
    New style effects

--- frontend/src/components/student/Components/Skills/Skills.js
+++ frontend/src/components/student/Components/Skills/Skills.js
@@ -5,7 +5,7 @@ const skills = props => {
     return (
         <React.Fragment>
             {
-            props.details.Edit?<Button as="input" className={classes.Input} value={props.details.SkillName} onChange={props.changeFn} onKeyPress={props.changeFn} onBlur={props.removeEdit}/>:
+            props.details.Edit?<Button as="input" autoFocus={true} className={classes.Input} value={props.details.SkillName} onChange={props.changeFn} onKeyPress={props.changeFn} onBlur={props.removeEdit}/>:
             <Button className={classes.Skill} onClick={props.remove} >{props.details.SkillName}</Button>
             }
         </React.Fragment>
