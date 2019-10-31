9a90a88cc443bd09b9f89fdebcc423a10727d6a8
Joshua Oliver <j.oliverc18@gmail.com>
2019-10-31 20:01:43 +0800

    Fixed elements position on AJ

--- frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
+++ frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
@@ -22,7 +22,7 @@ class AppliedJobs extends Component {
                 <br />
 
                 <Card md={{ span: 12 }} className={classes.Card}>
-                    <Card.Body>
+                    <Card.Body style={{ height: '185px' }}>
                         <Card.Title>
                             <Row className={classes.Row} style={{ fontWeight: '600' }}>Software Developer</Row>
                             <Row className={classes.Row}>Accenture</Row>
@@ -35,16 +35,23 @@ class AppliedJobs extends Component {
                         <Card.Text className={classes.Description}>
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </Card.Text>
                     </Card.Body>
-                        <Card.Footer className={classes.Footer}>
-                        <Col md={{ span: 2, offset: 10 }}>
-                            <span className={classes.Star} onClick={this.toggle}>
-                                {active ? fav : unfav}
-                            </span>
+                    <Card.Footer className={classes.Footer}>
+                        {/* <Col md={{ span: 12 }}> */}
+                        <div className={classes.ButtonDiv}>
                             <Button className={classes.Applied} disabled >Applied</Button>
-                        </Col>
+                        </div>
+                            <div className={classes.StarDiv}>
+                                <span className={classes.Star} onClick={this.toggle}>
+                                    {active ? fav : unfav}
+                                </span>
+                            </div>
+                        {/* </Col> */}
                     </Card.Footer>
                 </Card>
 
