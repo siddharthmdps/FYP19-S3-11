81201711c12abe944fe4e33a04adc0a207223e53
Joshua Oliver <j.oliverc18@gmail.com>
2019-11-02 00:06:58 +0800

    Testing timeline logic

--- frontend/src/components/student/Container/ViewProfile/ViewProfile.js
+++ frontend/src/components/student/Container/ViewProfile/ViewProfile.js
@@ -222,22 +222,32 @@ class Profile extends Component {
                                 })}
                             </Timeline>
                         </CardColumns> */}
+                        
                         <body >
                             <VerticalTimeline>
-                                <VerticalTimelineElement
-                                    className="vertical-timeline-element--work"
-                                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                                    date="2011 - present"
-                                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                    icon={<WorkIcon />}
-                                >
-                                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                                    <p>
-                                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                                    </p>
-                                </VerticalTimelineElement>
+                                {this.state.Education.map(educationDetail => {
+                                    return (
+                                        <VerticalTimelineElement
+                                            className="vertical-timeline-element--work"
+                                            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                                            date={educationDetail.StartDate + " - " + educationDetail.EndDate}
+                                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                            icon={<WorkIcon />}
+                                        >
+                                            <h3 className="vertical-timeline-element-title">{educationDetail.Degree + " in " +
+                                            educationDetail.FieldOfStudy + " (" +
+                                            educationDetail.Major + ")"}</h3>
+                                            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                                            <p>
+                                                {educationDetail.University}
+                                                <br/>
+                                                {educationDetail.GPA}
+                                            </p>
+                                        </VerticalTimelineElement>
+                                    );
+                                })}
+
                                 <VerticalTimelineElement
                                     className="vertical-timeline-element--work"
                                     date="2010 - 2011"
