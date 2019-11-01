9341ddbb04413b39666cb8c8889d949aa77c8645
Zhiting <35999032+ZhitingChia@users.noreply.github.com>
2019-11-01 14:25:17 +0800

    WIP

--- frontend/src/components/employer/JobView/EmpJobCard.js
+++ frontend/src/components/employer/JobView/EmpJobCard.js
@@ -1,18 +1,8 @@
 //These are components used by EmpJobView.js.
 
+//just backing up
 import React from 'react';
 import '../ProfileView/Card.css';
-
-/*const BaseCard =(props)=> {
-    return (
-            <div className="card mar15">
-                <div className="card-body">
-                   {props.children}
-                </div>
-            </div>
-            
-    )
-};*/
 
 const BaseCard =(props)=> {
     return (
@@ -23,26 +13,13 @@ const BaseCard =(props)=> {
     )
 };
 
-//original
-/*const EmpJobCard =(props)=> {
-    return (
-            <BaseCard>
-                <h5 className ="card-title">{props.title}</h5>
-                <p className="card-text"> {props.companyName}</p>
-                
-                <p className = "card-text"> {props.children}</p>
-                <p className ="card-text">{props.reqSkills}</p>
-            </BaseCard>
-            
-    )
-};*/
-
 const EmpJobCard =(props)=> {
     return (
             <div className = "col-sm-8 mx-auto">
                 <BaseCard>
                     <div className="card-header">
-                        <h5>{props.title}</h5>
+                        <h5>{props.title}<i class="fas fa-edit"></i></h5>
+
                     </div>
                 
                     <ul class="list-group list-group-flush">
@@ -58,11 +35,47 @@ const EmpJobCard =(props)=> {
 
 const EmpAppCard = (props) =>{
     return (
-        <div className= "col-sm-4">
+        <div className= "col-sm-8">
         <BaseCard>
             
                 <div className="card-body">
                     <h5 className ="card-title">{props.appName}</h5>
+                    <p className = "card-text">
+                        <ul>
+                            <li>{props.degree} </li>
+                            <li>{props.university}</li>
+                            <li>{props.gpa}</li>
+                        </ul>
+                    </p>
+
+                    <p className="card-text">
+                        <ul>
+                            <li>{props.position}</li>
+                            <li>{props.company}</li>
+                        </ul>
+
+                    </p>
+
+                    <p className= "card-text">
+                        <ul>
+                            <li>{props.awardName}</li>
+                            <li>{props.awardDate}</li>
+                        </ul>
+                    </p>
+
+                    <p className = "card-text">
+                        <ul>
+                            <li>{props.certName}</li>
+                            <li>{props.certABody}</li>
+                        </ul>
+                    </p>
+
+                    <p className= "card-text">
+                        <ul>
+                            <li>{props.projectTitle}</li>
+                        </ul>
+                    </p>
+
                     <p className="card-text"> {props.children}</p>
                     
                     <p className = "card-text"> {props.appSkills}</p>
@@ -73,9 +86,6 @@ const EmpAppCard = (props) =>{
     </div>
     )
 }
-
- 
-
 
 export {BaseCard,EmpAppCard};
 export default EmpJobCard;
