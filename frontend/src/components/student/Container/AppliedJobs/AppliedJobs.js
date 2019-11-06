f6326b73e481b2dc4f736ce4733ef93962fbd0a2
Siddharth SIngh <siddharthmdps@gmail.com>
2019-11-06 18:53:10 +0800

    Pagination update

--- frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
+++ frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, Button, Alert,Pagination } from 'react-bootstrap';
+import { Container, Row, Col, Card, Alert,Pagination } from 'react-bootstrap';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import classes from './AppliedJobs.module.css';
 
 import Axios from 'axios';
@@ -17,6 +21,35 @@ class AppliedJobs extends Component {
 
     toggle = () => this.setState({ "active": !this.state.active });
 
+    // pagination = () => {
+    //     let temp = [];
+    //     let number = this.state.AppliedJobs.length/this.state.jobsPerPage;
+    //     if(this.state.AppliedJobs.length%this.state.jobsPerPage !== 0)
+    //         number++;
+        
+    //     for (let index=1; index<=number; index++){
+    //         temp.push(
+    //         <Pagination.Item key={index} active={index === this.state.pageNo} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
+    //             {index}
+    //         </Pagination.Item>
+    //         );
+    //     }
+    //     this.setState({pageItems: temp});
+    // };
+
+    // refreshPagination = (newActive, number) => {
+    //     let temp = [];
+        
+    //     for (let index=1; index<=number; index++){
+    //         temp.push(
+    //         <Pagination.Item key={index} active={index === newActive} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
+    //             {index}
+    //         </Pagination.Item>
+    //         );
+    //     }
+    //     this.setState({pageItems: temp});
+    // };
+
     pagination = () => {
         let temp = [];
         let number = this.state.AppliedJobs.length/this.state.jobsPerPage;
@@ -24,11 +57,20 @@ class AppliedJobs extends Component {
             number++;
         
         for (let index=1; index<=number; index++){
-            temp.push(
-            <Pagination.Item key={index} active={index === this.state.pageNo} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
-                {index}
-            </Pagination.Item>
-            );
+            if(index === this.state.pageNo){
+                temp.push(
+                <Button key={index} style={{backgroundColor: '#FFCA28', color: 'black'}} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number) }}>
+                    {index}
+                </Button>
+                );
+            }
+            else{
+                temp.push(
+                    <Button key={index} style={{backgroundColor: '#fff', color: 'black'}} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number) }}>
+                        {index}
+                    </Button>
+                    );
+            }
         }
         this.setState({pageItems: temp});
     };
@@ -37,11 +79,25 @@ class AppliedJobs extends Component {
         let temp = [];
         
         for (let index=1; index<=number; index++){
-            temp.push(
-            <Pagination.Item key={index} active={index === newActive} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
-                {index}
-            </Pagination.Item>
-            );
+            if(index === newActive){
+                temp.push(
+                <Button key={index} style={{backgroundColor: '#FFCA28', color: 'black'}} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number) }}>
+                    {index}
+                </Button>
+                );
+            }
+            else{
+                temp.push(
+                    <Button key={index} style={{backgroundColor: '#fff', color: 'black'}} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number) }}>
+                        {index}
+                    </Button>
+                    );
+            }
+            // temp.push(
+            // <Pagination.Item key={index} active={index === newActive} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
+            //     {index}
+            // </Pagination.Item>
+            // );
         }
         this.setState({pageItems: temp});
     };
@@ -77,6 +133,19 @@ class AppliedJobs extends Component {
                 })} 
                 <br />
                 <Pagination size="lg">{this.state.pageItems}</Pagination>
+                <br />
+                <Grid item>
+            <ButtonGroup
+              color="secondary"
+              size="large"
+              aria-label="large outlined secondary button group"
+            >
+                {this.state.pageItems}
+              {/* <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button> */}
+            </ButtonGroup>
+          </Grid>
             </Container>
         );
     }
