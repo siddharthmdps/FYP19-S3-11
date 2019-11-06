db0df664fa65cbe1ac5d61226fc3c0620f1dac04
Siddharth SIngh <siddharthmdps@gmail.com>
2019-11-06 16:32:52 +0800

    Applied jobs pagination

--- frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
+++ frontend/src/components/student/Container/AppliedJobs/AppliedJobs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Alert,Pagination } from 'react-bootstrap';
 import classes from './AppliedJobs.module.css';
 
 import Axios from 'axios';
@@ -11,16 +11,47 @@ class AppliedJobs extends Component {
         "AppliedJobs": [],
         "active": false,
         "jobsPerPage": 5,
-        "pageNo": 1
+        "pageNo": 1,
+        "pageItems": []
     };
 
     toggle = () => this.setState({ "active": !this.state.active });
+
+    pagination = () => {
+        let temp = [];
+        let number = this.state.AppliedJobs.length/this.state.jobsPerPage;
+        if(this.state.AppliedJobs.length%this.state.jobsPerPage !== 0)
+            number++;
+        
+        for (let index=1; index<=number; index++){
+            temp.push(
+            <Pagination.Item key={index} active={index === this.state.pageNo} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
+                {index}
+            </Pagination.Item>
+            );
+        }
+        this.setState({pageItems: temp});
+    };
+
+    refreshPagination = (newActive, number) => {
+        let temp = [];
+        
+        for (let index=1; index<=number; index++){
+            temp.push(
+            <Pagination.Item key={index} active={index === newActive} onClick={() => { this.setState({pageNo: index}); this.refreshPagination(index, number)}} className={classes.PageItem}>
+                {index}
+            </Pagination.Item>
+            );
+        }
+        this.setState({pageItems: temp});
+    };
 
     componentDidMount(){
         Axios.get("http://localhost:3000/AppliedJobs")
             .then(receivedData =>{
                 this.setState({ AppliedJobs: receivedData.data });
                 console.log(receivedData.data);
+                this.pagination()
             });
     }
 
@@ -45,7 +76,7 @@ class AppliedJobs extends Component {
                     );
                 })} 
                 <br />
-                
+                <Pagination size="lg">{this.state.pageItems}</Pagination>
             </Container>
         );
     }
