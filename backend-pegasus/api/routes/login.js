1ab602cf751943ec777c8d6e6f1976b3f4b79bf7
Poh <minmarnoo@gmail.com>
2019-11-09 10:46:07 +0800

    fixing login and app.js

--- backend-pegasus/api/routes/login.js
+++ backend-pegasus/api/routes/login.js
@@ -23,8 +23,15 @@ const login = (req,res) => {
                         res.status(500).json({ message: error })
                     }
                     if(rows && rows.length > 0) {
-                        var response = '{' + usertypeid + ' : ' + rows[0]["id"] + '}' 
-                        res.json(response)
+                        //var response = '{' + usertypeid + ' : ' + rows[0]["id"] + '}' 
+
+                        let result = rows[0]
+                        delete result['password']
+
+                        res.json({
+                            message: 'success',
+                            body: result
+                        })
                     }
                     else if( !rows || rows.length == 0 ) {
                         res.status(200).json({
