d5b829339564422de2795f279f7ec06d821fc188
Siddharth SIngh <siddharthmdps@gmail.com>
2019-11-14 04:29:36 +0800

    Login completed

--- frontend/src/components/common_assets/Login/Login.js
+++ frontend/src/components/common_assets/Login/Login.js
@@ -1,12 +1,80 @@
 import React, {Component} from 'react';
 import classes from './Login.module.css';
 
+import apiURL from '../../../config';
+
+import auth from '../../../utils/auth';
+
 import { Container, Row, Col, Card } from 'react-bootstrap';
 
-import { FormControl, FormControlLabel, Radio , TextField, Grid, RadioGroup, Fab, Button} from '@material-ui/core';
+import { FormControlLabel, Radio , TextField, Grid, RadioGroup, Fab, Button} from '@material-ui/core';
 import { Person as PersonIcon, Lock as LockIcon, ExitToApp as ExitAppIcon } from '@material-ui/icons';
 
 class Login extends Component {
+
+    state = {
+        userName: "",
+        password: "",
+        userType: "",
+        loading : false
+    }
+
+    validateUser = (cb) => {
+        // const username = document.getElementById("input-username").value.toLowerCase()
+        // const password = document.getElementById("input-password").value
+        // const usertype = document.getElementById("input-usertype").value
+
+        const loginParticulars = { "username" : this.state.userName.toLowerCase(), "password" : this.state.password, "usertype" : this.state.userType }
+        
+        
+
+        if(this.state.userName !== "" && this.state.password !== "" && this.state.userType !== "") {
+            this.setState({loading : true})
+            auth.login(loginParticulars, (loginResult) => {
+                this.setState({loading : false})
+
+                // if login is successful, route the user to their home page
+                if(loginResult){
+                    window.location = `/${this.state.userType}`
+                } 
+                else 
+                    alert('Login failed, please try again.')
+            })
+        }
+        else {
+            alert ('Please fill in all the required fields')
+        }
+        
+    }
+
+    setLoadingSpin = () => {
+        if (this.state.loading) {
+            return (
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            )
+        }
+        else return null
+    }
+
+    changeUserNameHandler = event => {
+        console.log(event.target.value);
+        this.setState({userName: event.target.value});
+    }
+
+    changePasswordHandler = event => {
+        console.log(event.target.value);
+        this.setState({password: event.target.value});
+    }
+
+    changeUserTypeHandler = event => {
+        console.log(event.target.value);
+        this.setState({userType: event.target.value});
+    }
+
     render(){
         return(
             <Container fluid className={classes.Login}>
@@ -23,7 +91,7 @@ class Login extends Component {
                                         <PersonIcon />
                                     </Grid>
                                     <Grid item xs={10}>
-                                        <TextField id="input-with-icon-grid" label="User ID" fullWidth />
+                                        <TextField id="input-username" label="User ID" value={this.state.userName} onChange={event => this.changeUserNameHandler(event)} fullWidth />
                                     </Grid>
                                 </Grid>
                                 <br />
@@ -32,31 +100,31 @@ class Login extends Component {
                                         <LockIcon />
                                     </Grid>
                                     <Grid item xs={10}>
-                                        <TextField id="input-with-icon-grid" type="password" label="Password" fullWidth />
+                                        <TextField id="input-password" type="password" label="Password" value={this.state.password} onChange={event => this.changePasswordHandler(event)} fullWidth />
                                     </Grid>
                                 </Grid>
 
                                 <br />
                                 <RadioGroup aria-label="position" name="position" 
-                                // value={value} onChange={handleChange} 
+                                value={this.state.userType} onChange={event => this.changeUserTypeHandler(event)} 
                                 row>
 
                                     <FormControlLabel
-                                        value="Student"
+                                        value="student"
                                         control={<Radio color="primary" />}
                                         label="Student"
                                         labelPlacement="start"
                                     />
 
                                     <FormControlLabel
-                                        value="Employer"
+                                        value="employer"
                                         control={<Radio color="primary" />}
                                         label="Employer"
                                         labelPlacement="start"
                                     />
 
                                     <FormControlLabel
-                                        value="Admin"
+                                        value="admin"
                                         control={<Radio color="primary"/>}
                                         label="Admin"
                                         labelPlacement="start"
@@ -64,17 +132,17 @@ class Login extends Component {
 
                                 </RadioGroup>
 
-                                <Fab variant="extended" color="primary" aria-label="add" className={classes.LoginButton}>
+                                <Fab variant="extended" color="primary" aria-label="add" className={classes.LoginButton} onClick={this.validateUser}>
                                     <ExitAppIcon />
                                     Login
                                 </Fab>
                                 <br />
+                                <this.setLoadingSpin/>
                                 <br />
                                 New to Pegasus? 
-                                <Button color="primary" className={classes.button}>
+                                <Button color="primary" className={classes.button} href="/signup">
                                     Join Now
                                 </Button>
-
                             </Card.Body>
                         </Card>
                     </Col>
