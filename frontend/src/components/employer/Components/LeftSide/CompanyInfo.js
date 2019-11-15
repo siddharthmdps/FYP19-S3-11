bb9bd1fbd3d88b3304ab397e85b277b5bcc9f039
Joshua Oliver <j.oliverc18@gmail.com>
2019-11-15 15:31:26 +0800

    Adjsuted ViewProfile

--- frontend/src/components/employer/Components/LeftSide/CompanyInfo.js
+++ frontend/src/components/employer/Components/LeftSide/CompanyInfo.js
@@ -7,7 +7,8 @@ const companyInfo = props => (
         <ResponsiveEmbed aspectRatio="1by1">
             <Image src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-P3iJ7YoAPkY%2FT5e57OJGikI%2FAAAAAAAAA38%2FRTHs9cmhndQ%2Fs1600%2Fapple-logo-clouds.jpg&f=1&nofb=1" className={classes.Image} />
         </ResponsiveEmbed>
-        <p className="companyName">{props.companyName}</p>
+        <br/>
+        <p className={classes.CompanyName}>{props.companyName}</p>
     </React.Fragment>
 );
 
