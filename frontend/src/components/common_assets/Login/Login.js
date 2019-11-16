042bf45b48a227ad453d94790b762913b2296974
Joshua Oliver <j.oliverc18@gmail.com>
2019-11-16 18:03:37 +0800

    New login BG

--- frontend/src/components/common_assets/Login/Login.js
+++ frontend/src/components/common_assets/Login/Login.js
@@ -133,7 +133,8 @@ class Login extends Component {
 
                                 </RadioGroup>
 
-                                <Fab variant="extended" color="primary" aria-label="add" className={classes.LoginButton} onClick={this.validateUser}>
+                                <Fab    style={{backgroundColor: 'lightgreen', width: '80%'}} 
+                                variant="extended" color="primary" aria-label="add" className={classes.LoginButton} onClick={this.validateUser}>
                                     <ExitAppIcon />
                                     Login
                                 </Fab>
@@ -141,7 +142,7 @@ class Login extends Component {
                                 <this.setLoadingSpin/>
                                 <br />
                                 New to Pegasus? 
-                                <Button color="primary" className={classes.button} href="/signup">
+                                <Button style={{color: 'lightgreen'}}className={classes.button} href="/signup">
                                     Join Now
                                 </Button>
                             </Card.Body>
