0a0f4af733fc56874a33a6618602167e055e9438
Joshua Oliver <j.oliverc18@gmail.com>
2019-11-17 15:56:44 +0800

    Removed unnecessary row in footer

--- frontend/src/components/admin/Home.js
+++ frontend/src/components/admin/Home.js
@@ -3,17 +3,13 @@ import classes from './Home.module.css';
 
 export class Home extends Component {
     render() {
-        document.documentElement.style=
-        'height: 100%';
         return (
-            <React.Fragment>
-                <div className={classes.Home}>
-                    <div className={classes.HomeContent}>
-                        <h4>Welcome to Pegusus@SIM</h4>
-                        <h1>Admin Management Portal</h1>
-                    </div>
+            <div className={classes.Home}>
+                <div>
+                    <h4>Welcome to Pegusus@SIM</h4>
+                    <h1>Admin Management Portal</h1>
                 </div>
-            </React.Fragment>
+            </div>
         )
     }
 }
