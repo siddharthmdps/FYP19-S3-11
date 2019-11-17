d968a85587fc647456fd48da4adcc1c55c99129a
Joshua Oliver <j.oliverc18@gmail.com>
2019-11-17 15:30:18 +0800

    Repositioned footer

--- frontend/src/components/admin/Footer.js
+++ frontend/src/components/admin/Footer.js
@@ -3,19 +3,22 @@ import { MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const FooterPage = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      {/* <MDBContainer fluid className="text-center text-md-left">
+    <MDBFooter color="blue" className="font-small pt-4 mt-4" style={{position: 'absolute', bottom: '0', width: '100%'}}>
+      <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
 
 
         </MDBRow>
-      </MDBContainer> */}
+      </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright: Pegasus@SIM
         </MDBContainer>
       </div>
     </MDBFooter>
+    // <React.Fragment>
+
+    // </React.Fragment>
   );
 }
 
--- frontend/src/components/admin/Home.js
+++ frontend/src/components/admin/Home.js
@@ -3,10 +3,12 @@ import classes from './Home.module.css';
 
 export class Home extends Component {
     render() {
+        document.documentElement.style=
+        'height: 100%';
         return (
             <React.Fragment>
                 <div className={classes.Home}>
-                    <div>
+                    <div className={classes.HomeContent}>
                         <h4>Welcome to Pegusus@SIM</h4>
                         <h1>Admin Management Portal</h1>
                     </div>
