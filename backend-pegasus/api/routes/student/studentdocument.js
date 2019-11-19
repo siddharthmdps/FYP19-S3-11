577ea9c6cbc6b3b0a4eaad1b3a9792c487b5b173
Min San <52874852+minsan96@users.noreply.github.com>
2019-11-19 14:26:28 +0800

    studentdocument fixes

--- backend-pegasus/api/routes/student/studentdocument.js
+++ backend-pegasus/api/routes/student/studentdocument.js
@@ -1,56 +1,56 @@
 const {env, sha1, mysql, mypool} = require('../../util')
 
 const studentdocument = (req, res) => {
+        const studentid = req.body.StudentID;
+        const documentid = req.body.DocumentID;
+        const title = req.body.Title;
+        let link = req.body.Link;
+        const imagetype = req.body.ImageType;
+        console.log(req);
+        console.log(link);
 
-    for(var key in req.body) {
-        if(req.body.hasOwnProperty(key)) {
-            const studentid = req.body[key].studentid;
-            const documentid = req.body[key].DocumentID;
-            const title = req.body[key].Title;
-            const link = req.body[key].Link;
+        var foundduplicate = false;
 
-            var foundduplicate = false;
-
-            mypool.getConnection( (error, connection) => {
-                if(error) {
-                    connection.release()
-                    console.log(`Error getting mysql_pool connection: ${error}`)
-                    throw error
-                }
-                else {
-                    if(studentid && title) {      
-                        let queryString1 = `select * from pegasus.studentdocument where id = "${documentid}" and studentid = "${studentid}"` ;              
-                        let queryString2 = `INSERT INTO pegasus.studentdocument (studentid, title, link) values ("${studentid}", "${title}", "${link}")`
-                        connection.query(queryString1, (err, rows, fields) => {
-                            if(err) {
-                                res.status(500).json({ message: err })
-                            }
-                            if(rows.length > 0) {
-                                foundduplicate = true;
-                            }
-                            if(foundduplicate) {
-                                queryString2 = `UPDATE pegasus.studentdocument set title = "${title}", link = "${link}" where id = "${documentid}" and studentid = "${studentid}"`
-                            }
-                            connection.query(queryString2, (err, rows, fields) => {
-                                if(err) {
-                                    res.status(500).json({ message: err });
-                                }
-                            }) 
-                        }) 
-                    } else {
-                        res.status(400).json({
-                            message: "Bad Request! Invalid POST request!"
-                        });
-                    }
-            
-                }
+        mypool.getConnection( (error, connection) => {
+            if(error) {
                 connection.release()
-            } )
-        }
-    }
-    res.json({
-        message: "success"
-    })
+                console.log(`Error getting mysql_pool connection: ${error}`)
+                throw error
+            }
+            else {
+                if(studentid && title) {      
+                    let queryString1 = `select * from pegasus.studentdocument where id = "${documentid}" and studentid = "${studentid}"` ;              
+                    let queryString2 = `INSERT INTO pegasus.studentdocument (studentid, title, link, imagetype) values ("${studentid}", "${title}", "${link}", "${imagetype}")`
+                    connection.query(queryString1, (err, rows, fields) => {
+                        if(err) {
+                            res.status(500).json({ message: err })
+                        }
+                        if(rows.length > 0) {
+                            foundduplicate = true;
+                        }
+                        if(foundduplicate) {
+                            queryString2 = `UPDATE pegasus.studentdocument set title = "${title}", link = "${link}", imagetype = "${imagetype}" where id = "${documentid}" and studentid = "${studentid}"`
+                        }
+                        connection.query(queryString2, (err, rows, fields) => {
+                            if(err) {
+                                res.status(500).json({ message: err });
+                            }
+                            else {
+                                res.json({
+                                    message: "success"
+                                })
+                            }
+                        }) 
+                    }) 
+                } else {
+                    res.status(400).json({
+                        message: "Bad Request! Invalid POST request!"
+                    });
+                }
+        
+            }
+            connection.release()
+        } )
 }
 
 module.exports = studentdocument
