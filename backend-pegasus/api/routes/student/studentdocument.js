1a7bdd09372b0d43e3dc2db513cd50155b0a1e0c
Min San <52874852+minsan96@users.noreply.github.com>
2019-11-22 02:28:24 +0800

    student document changes to insert or update documents

--- backend-pegasus/api/routes/student/studentdocument.js
+++ backend-pegasus/api/routes/student/studentdocument.js
@@ -85,55 +85,78 @@ const studentdocument = (req, res) => {
           return res.status(500).json(err)
         }
         var studentid = req.body.StudentID;
+        const documentid = req.body.DocumentID;
+        const title = req.body.Title;
         var success = true;
-        let queryString1 = `delete from pegasus.studentdocument where studentid = "${studentid}"` ;  
-        mypool.getConnection( (error, connection) => {
-            connection.query(queryString1, (err, rows, fields) => { 
-                if(err) {
-                    return res.status(500).json({ err });
-                }
-            });
-            const documentid = req.body.DocumentID;
-            const title = req.body.Title;
-            var obj =  req.file;
-            var link = obj.buffer;
-            const imagetype = req.body.FileType;
-
-            var buffer = new Buffer.from(link).toString('base64');
+        var stringlink = req.body.Link;
+        console.log(req.body);
+        if(stringlink && typeof stringlink == "string") {
+            let queryString3 = `update pegasus.studentdocument set title = "${title}" where studentid = "${studentid}"` ;  
             mypool.getConnection( (error, connection) => {
-                if(error) {
-                    connection.release()
-                    console.log(`Error getting mysql_pool connection: ${error}`)
-                    throw error
-                }
-                else {
-                    if(studentid && title) {        
-                        var queryString2 = "INSERT INTO pegasus.studentdocument SET ?";
-                        var insert = {
-                            studentid: studentid,
-                            title: title,
-                            link: buffer,
-                            imagetype: imagetype
-                        };
-                        connection.query(queryString2, insert, (err, rows, fields) => {
-                            if(err) {
-                                res.status(500).json({ message: err });
-                                success = false;
-                            }
-                        })                         
-                        i++;
-                    } else {
-                        res.status(400).json({
-                            message: "Bad Request! Invalid POST request!"
-                        });
-                    }   
-                }
-            } )
-            if(success) {
-                res.json({message: "success"});
-            } 
-            connection.release()        
-        });
+                connection.query(queryString3, (err, rows, fields) => { 
+                    if(err) {
+                        res.status(500).json({ err });
+                    }
+                    else {
+                        res.json({message: "success"});
+                    }
+                });
+                connection.release() 
+            });
+        }
+        else {
+            var obj =  req.file ? req.file : stringlink;
+            var link = obj.buffer;
+            //console.log(link);
+            const imagetype = req.body.FileType;
+            let queryString1 = `delete from pegasus.studentdocument where studentid = "${studentid}"` ;  
+            mypool.getConnection( (error, connection) => {
+                connection.query(queryString1, (err, rows, fields) => { 
+                    if(err) {
+                        return res.status(500).json({ err });
+                    }
+                });
+
+                var buffer = new Buffer.from(link).toString('base64');
+                mypool.getConnection( (error, connection) => {
+                    if(error) {
+                        connection.release()
+                        console.log(`Error getting mysql_pool connection: ${error}`)
+                        throw error
+                    }
+                    else {
+                        if(studentid && title) {  
+                            console.log(studentid); 
+                            console.log(title);     
+                            var queryString2 = "INSERT INTO pegasus.studentdocument SET ?";
+                            var insert = {
+                                studentid: studentid,
+                                title: title,
+                                link: buffer,
+                                imagetype: imagetype
+                            };
+                            connection.query(queryString2, insert, (err, rows, fields) => {
+                                if(err) {
+                                    console.log(3);
+                                    res.status(500).json({ message: err });
+                                    success = false;
+                                }
+                            })   
+                        } else {
+                            console.log(4);
+                            success = false;
+                            res.status(400).json({
+                                message: "Bad Request! Invalid POST request!"
+                            });
+                        }   
+                    }
+                } )
+                if(success) {
+                    res.json({message: "success"});
+                } 
+                connection.release()        
+            });
+        }
         return true
     })    
 }
