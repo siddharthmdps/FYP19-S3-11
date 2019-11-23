d5de71e03199473c4e2f3f16b772564c6de83716
ZhitingChia <35999032+ZhitingChia@users.noreply.github.com>
2019-11-23 16:25:56 +0800

    update default image for companyInfo /LeftSide

--- frontend/src/components/employer/Components/LeftSide/CompanyInfo.js
+++ frontend/src/components/employer/Components/LeftSide/CompanyInfo.js
@@ -5,9 +5,9 @@ import classes from './CompanyInfo.module.css'
 const companyInfo = props => (
     <React.Fragment>
         <ResponsiveEmbed aspectRatio="1by1">
-            <Image src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-P3iJ7YoAPkY%2FT5e57OJGikI%2FAAAAAAAAA38%2FRTHs9cmhndQ%2Fs1600%2Fapple-logo-clouds.jpg&f=1&nofb=1" className={classes.Image} />
+            <Image src="default3.png" className={classes.Image} />
         </ResponsiveEmbed>
-        <br/>
+        <br />
         <p className={classes.CompanyName}>{props.companyName}</p>
     </React.Fragment>
 );
--- frontend/src/components/employer/Components/LeftSide/LeftSide.js
+++ frontend/src/components/employer/Components/LeftSide/LeftSide.js
@@ -12,7 +12,7 @@ const leftSide = (props) => {
             <Card className={classes.LeftSide}>
                 <Card.Body>
                     <ResponsiveEmbed aspectRatio="1by1">
-                        <Image src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-P3iJ7YoAPkY%2FT5e57OJGikI%2FAAAAAAAAA38%2FRTHs9cmhndQ%2Fs1600%2Fapple-logo-clouds.jpg&f=1&nofb=1" className={classes.Image} />
+                        <Image src="default3.png" className={classes.Image} />
                     </ResponsiveEmbed>
                     <p className="companyName">{props.companyName}</p>
                 </Card.Body>
