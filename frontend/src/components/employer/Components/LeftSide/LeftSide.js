52948141e35a7fea12b6cbe3617ffc467265e5f8
ZhitingChia <35999032+ZhitingChia@users.noreply.github.com>
2019-11-23 18:18:10 +0800

    Remove dud statistics added link to view jobs

--- frontend/src/components/employer/Components/LeftSide/LeftSide.js
+++ frontend/src/components/employer/Components/LeftSide/LeftSide.js
@@ -2,7 +2,7 @@ import React from 'react';
 import StatBox from './StatBox'
 import classes from './LeftSide.module.css';
 import { Card, Form, Col, Image, ListGroup, ListGroupItem, ResponsiveEmbed } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom'
 //This component is the employer's control panel.
 //It contains a Profile Container and a statboard.
 
@@ -20,22 +20,25 @@ const leftSide = (props) => {
                 <Card.Body>
                     <Form.Row >
                         <Form.Group as={Col} lg={4} sm={12}>
-                            <StatBox number={props.numOfJobs} desc="Jobs" />
+                            <StatBox number="" desc="" />
                         </Form.Group>
                         <Form.Group as={Col} lg={4} sm={12}>
-                            <StatBox number="62" desc="Applicants" />
+                            <Link to={`/employer/viewjobs`} style={{ textDecoration: 'none' }}>
+                                <StatBox number={props.numOfJobs} desc="Jobs" />
+                            </Link>
                         </Form.Group>
                         <Form.Group as={Col} lg={4} sm={12}>
-                            <StatBox number="333" desc="Applications" />
+                            <StatBox number="" desc="" />
                         </Form.Group>
+
                     </Form.Row>
 
                 </Card.Body>
-                <ListGroup className="list-group-flush" className={classes.CentralContent}>
+                {/* <ListGroup className="list-group-flush" className={classes.CentralContent}>
                     <ListGroupItem className={classes.Clickable}> Preview Company Profile </ListGroupItem>
                     <ListGroupItem onClick={props.pjHandler} className={classes.Clickable}>View Jobs</ListGroupItem>
                     <ListGroupItem className={classes.Clickable}>View Applicants</ListGroupItem>
-                </ListGroup>
+                </ListGroup> */}
                 {/* <button type="button" className={classes.BtnNoOutline} >Preview Company Profile</button>
 
                     <button onClick={props.pjHandler} type="button" className={classes.BtnNoOutline} >Post Jobs</button>
